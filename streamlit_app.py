fb6fe8dd59a07a8ffd078da42c3ed8d6274972bc
Sirishak29 <sirishak.dwh@gmail.com>
2026-01-20 12:18:55 -0500

    fix 12

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         st_df = st.dataframe(data=smoothiefroot_response.json(), width="stretch")
-        response = requests.get(smoothiefroot_response)
+        response = smoothiefroot_response
     
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order )
