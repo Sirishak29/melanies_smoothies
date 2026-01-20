5b4c6754567f2d8515f9569b59200c0df56d7e01
Sirishak29 <sirishak.dwh@gmail.com>
2026-01-20 10:20:14 -0500

    adding json

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,5 +50,5 @@ st.stop()
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
         
